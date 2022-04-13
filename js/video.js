1002d5092d4bff66400fd3bf2e1ce0cb928b882f
yuexu22 <97536845+yuexu22@users.noreply.github.com>
2022-04-13 14:22:42 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function() {
 document.querySelector('#play').addEventListener("click", function() {
 	console.log('Play Video');
 	video.play();
-	document.querySelector("#volumn").innerHTML = video.volume * 100 + "%";
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
 
 });
 
